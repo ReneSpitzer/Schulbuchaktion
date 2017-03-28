d9a6d1a1e9a5a5c109f3657c9fb7c23b2ac1edc4
ReneSpitzer <rene.spitzer16@gmail.com>
2017-03-28 11:57:07 +0200

    logingpage und beans

--- src/java/at/htlpinkafeld/gui/UserBean.java
+++ src/java/at/htlpinkafeld/gui/UserBean.java
@@ -5,10 +5,18 @@
  */
 package at.htlpinkafeld.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author rene-_000
  */
 public class UserBean {
+    private User user;
+    private List<User> = new ArrayList();
     
+    public UserBean(){
+        
+    }
 }
