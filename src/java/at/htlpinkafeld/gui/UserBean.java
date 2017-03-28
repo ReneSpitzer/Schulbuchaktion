cb292e44b0f082bcb9560d761545b6550c261368
ReneSpitzer <rene.spitzer16@gmail.com>
2017-03-28 11:47:34 +0200

    Project created

---
+++ src/java/at/htlpinkafeld/gui/UserBean.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.htlpinkafeld.gui;
+
+/**
+ *
+ * @author rene-_000
+ */
+public class UserBean {
+    
+}
